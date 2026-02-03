c37cc828cba3534f6999e820f7a5f8388457a3f3
saket5743 <saketkumar.93864@gmail.com>
2026-02-03 21:03:17 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from 'dotenv';
+dotenv.config();
+
+// const authRoutes = require("./routes/auth.routes");
+// const customerRoutes = require("./routes/customer.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/customers", customerRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`CRM API running on port ${process.env.PORT}`);
+});
